72eb4c97687bf88c549301089bb86a1a5137ef1f
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-24 13:07:20 -0700

    [ADD] Empezando proyecto, maquetado el header y agregala logica basica para los estilos

---
+++ src/components/Header.tsx
@@ -0,0 +1,70 @@
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+
+export default function Header() {
+
+    const {pathname} = useLocation()
+
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
+    return (
+        <header className={isHome ? 'bg-header' : 'bg-slate-800'}>
+            <div className='mx-auto container px-5 py-16'>
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <img className='w-32' src="/logo.svg" alt="logotipo" />
+                    </div>
+                    <nav className="flex gap-4">
+                        <NavLink 
+                            to='/'
+                            className={({isActive}) => 
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                            }
+                        >Inicio</NavLink>
+                        <NavLink 
+                            to='/favorites'
+                            className={({isActive}) => 
+                                isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                            }
+                        >Favoritos</NavLink>
+                    </nav>
+                </div>
+                {isHome && (
+                    <form className="bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6 md:w-1/2 2xl:1/3" action="">
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="ingedient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
+                            <input 
+                                id="ingedient"
+                                name="ingedient"
+                                type="text" 
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o ingrediente. Ej. Vodka, Tequila."
+                                />
+                        </div>
+
+                        <div className="space-y-4">
+                            <label 
+                                htmlFor="category"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoría</label>
+                            <select 
+                                name="category" 
+                                id="category"
+                                className="p-3 w-full rounded-lg focus:outline-none">
+                                <option value="">-- Seleccione ---</option>
+                            </select>
+                        </div>
+                        <input 
+                            type="submit"
+                            value='Buscar Recetas'
+                            className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" 
+                        />
+                    </form>
+                )}
+            </div>
+        </header>
+    )
+}
